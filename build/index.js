94c757a8a14bcd3ddf81d9810d5c92b98b097e65
Muhammad Hamza <108857251+itxhamza@users.noreply.github.com>
2024-01-20 21:28:50 +0500

    commit changes

---
+++ build/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const next = require('next');
+const path = require('path');
+
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
